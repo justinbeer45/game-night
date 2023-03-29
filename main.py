a1c56cbc796ba3dd8d9db38bc1feedd52dac6190
Justin Wheeler <justin.wheeler0914@gmail.com>
2023-03-29 17:53:09 -0600

    - ask for game played tonight

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-
+import time
 
 def welcome():
-    print("Welcome. Let's decide if we will play tonight's game again.")
+    print("\nWelcome. Let's decide if we will play tonight's game again.\n")
+
+
+def get_game_name():
+    blank = True
+    while blank:
+        game = input("What was tonight's game:")
+        if game != '':
+            blank = False
+            continue
+        print("\tYou will need to enter a name.\n")
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     welcome()
+    get_game_name()
