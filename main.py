84db06e8525c80f9b33118efe9468c1a5580461f
Justin Wheeler <justin.wheeler0914@gmail.com>
2023-03-29 17:30:22 -0600

    - add welcome message

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(message):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'{message}')  # Press Ctrl+F8 to toggle the breakpoint.
+def welcome():
+    print("Welcome. Let's decide if we will play tonight's game again.")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Game night begins.')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    welcome()
